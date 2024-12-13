a4da180be7a1940bbca20b7b3feaed4a24dc5b4a
Kayode Osagbemi <tok2kayode@gmail.com>
2024-12-13 13:36:56 -0500

    Add project files.

---
+++ LearnPython/LearnPython.py
@@ -0,0 +1,20 @@
+# This is my first program. I have decided to learn Python and then use it for any programming interviews for big tech
+# and also use it to pass MSc in Financial Engineering. Perhaps I will pivot to building all my side projects in Python. Lol
+
+# name = input("What is your name ")
+# print("Hello world ", name)
+# length = 4
+# namelen=len(name)
+# if namelen < length:
+#     print("the lenght of your name is ", namelen, " is shorter than 4")
+# else:
+#     print("The length of your name is just fine and it is ", namelen)
+
+fd=open('C:\\Users\\HP\\Documents\\PERSONAL\\personal\\CV\\Base CV - Revamped With New Knowledge\\Banking-Ready CVs\\C# Focus\\Kayode_Osagbemi_NET and Angular.pdf',"r")
+
+fd.close()
+
+x=type(float(3))
+print(x)
+x, y, z = "Orange", "Banana", "Cherry"
+print(x, y, z)
